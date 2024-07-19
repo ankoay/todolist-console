908fa79a7498b800444aba5a801cfa06d8c7ec90
YruloSama <irielahrandri@gmail.com>
2024-07-19 08:47:43 +0300

    ajout code

---
+++ upef/metier/MarkTaskCompleted.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MarkTaskCompleted  implements TaskOperation {
+    private TaskManager taskManager;
+    private Scanner scanner;
+
+    public MarkTaskCompleted(TaskManager taskManager, Scanner scanner) {
+        this.taskManager = taskManager;
+        this.scanner = scanner;
+    }
+
+    public void process() {
+        System.out.print("Entrez le numéro de la tâche à marquer comme complétée : ");
+        int index = scanner.nextInt();
+        if (index > 0 && index <= taskManager.getTasks().size()) {
+            Task task = taskManager.getTasks().get(index - 1);
+            task.setCompleted(true);
+            System.out.println("La tâche a été marquée comme complétée.");
+        } else {
+            System.out.println("Numéro de tâche invalide.");
+        }
+    }
+}
